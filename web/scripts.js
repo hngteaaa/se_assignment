4a5b21fbcd7000a3c90a9576355143a657ccee45
hngteaaa <64629561+hngteaaa@users.noreply.github.com>
2021-11-03 19:31:21 +0700

    Add files via upload

---
+++ web/scripts.js
@@ -0,0 +1,38 @@
+        // Accordion 
+        function myAccFunc() {
+            var x = document.getElementById("demoAcc");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+
+        // Click on the "Jeans" link on page load to open the accordion for demo purposes
+        document.getElementById("myBtn").click();
+
+
+        // Open and close sidebar
+        function w3_open() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
+
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
+
+        function openFood(evt, foodName) {
+            var i, x, tablinks;
+            x = document.getElementsByClassName("food");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablink");
+            for (i = 0; i < x.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" w3-black", "");
+            }
+            document.getElementById(foodName).style.display = "block";
+            evt.currentTarget.className += " w3-black";
+        }
